69e3c9d9f7f53bb4f62d0c36096c37eb8d68c5fc
11Nim <brunolfar@hotmail.com>
2020-12-18 11:45:26 -0300

    primeira ideia do fan inimigo, depois vejo animação e tals

---
+++ GradiusRemake/Assets/scripts/facnenemy.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class facnenemy : MonoBehaviour
+{
+    public Vector2 fanDirection = Vector2.zero;
+    private float fanSpeed;
+    private bool isStarting;
+    private Rigidbody2D fan;
+    
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        fan = this.GetComponent<Rigidbody2D>();
+        isStarting = true;
+        fanDirection.x -= 1;
+        fanSpeed = 3;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isStarting)
+        {
+            //this.fan.position = {30,15};
+            isStarting = false;
+        }
+        fan.MovePosition(this.fan.position + fanDirection.normalized * fanSpeed * Time.deltaTime);
+    }
+}
