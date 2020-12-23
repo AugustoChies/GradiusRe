1e4508c4c9d810b9bc8c47dac31cdf0f4c141c56
AugustoChies <guto_chies@hotmail.com>
2020-12-22 23:54:43 -0300

    Shootin with regular shots done. (Tá lindo)

--- GradiusRemake/Assets/scripts/ShipShooting.cs
+++ GradiusRemake/Assets/scripts/ShipShooting.cs
@@ -4,18 +4,53 @@ using UnityEngine;
 
 public class ShipShooting : MonoBehaviour
 {
-    public List<GameObject> inactiveShots,activeShots;
-    
+    public List<Shot> inactiveShots,activeShots;
+    public ControlsObj controls;
+    protected float continuousShotDelay;//Holding fire button causes slower continuous shooting
+    public float holdFireTime;
+
+    public GameObject regularshot;
+    protected Vector3 stashPos = new Vector3(0, 100, 0);
+    public Transform fireOriginPos;
     // Start is called before the first frame update
     void Awake()
     {
-        inactiveShots = new List<GameObject>();
-        activeShots = new List<GameObject>();
+        activeShots = new List<Shot>();
+        inactiveShots = new List<Shot>();
+        for (int i = 0; i < 6; i++)
+        {
+            GameObject token;
+            token = Instantiate(regularshot,stashPos,Quaternion.identity);
+            token.GetComponent<Shot>().stashList = inactiveShots;
+            token.GetComponent<Shot>().stashPos = stashPos;
+            inactiveShots.Add(token.GetComponent<Shot>());
+        }        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(controls.a))
+        {
+            continuousShotDelay = 0;
+            Shoot();
+        }
+        if(Input.GetKey(controls.a))
+        {
+            continuousShotDelay += Time.deltaTime;
+        }
+        if (continuousShotDelay >= holdFireTime)
+        {
+            continuousShotDelay = 0;
+            Shoot();
+        }
+    }
+
+    public void Shoot()
+    {
+        if(activeShots.Count < 2 && inactiveShots.Count > 0)
+        {
+            inactiveShots[0].Activate(fireOriginPos.position, activeShots);
+        }
     }
 }
--- GradiusRemake/Assets/scripts/Shot.cs
+++ GradiusRemake/Assets/scripts/Shot.cs
@@ -11,9 +11,14 @@ public abstract class Shot : MonoBehaviour
 
     protected Rigidbody2D rb;
 
+
+    public List<Shot> activeList,stashList;
+    public Vector3 stashPos;
+
     protected void Awake()
     {
         isActive = false;
+        rb = this.GetComponent<Rigidbody2D>();
     }
 
 
@@ -24,5 +29,30 @@ public abstract class Shot : MonoBehaviour
     }
 
     public abstract void Move();
-    
+
+    public void Activate(Vector3 newPosition,List<Shot> newActiveList)
+    {
+        activeList = newActiveList;
+        isActive = true;
+        rb.position = newPosition;
+        activeList.Add(this);
+        stashList.Remove(this);
+    }
+
+    public void Deactivate()
+    {
+        isActive = false;
+        rb.position = stashPos;
+        activeList.Remove(this);
+        stashList.Add(this);
+    }
+
+    protected void OnTriggerEnter2D(Collider2D collision)
+    {        
+        if(collision.gameObject.layer == 8) //camborder
+        {
+            Deactivate();
+        }
+    }
+
 }
