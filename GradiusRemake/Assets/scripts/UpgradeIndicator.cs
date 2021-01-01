1e09eedf1ae95ef617b0bd28beb823b476f7aa3f
AugustoChies <guto_chies@hotmail.com>
2020-12-31 21:28:01 -0300

    Implementing upgrade UI, commiting before changas to image

---
+++ GradiusRemake/Assets/scripts/UpgradeIndicator.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpgradeIndicator : MonoBehaviour
+{
+    public Sprite[] sprites;
+    protected Image myImage;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        myImage = this.GetComponent<Image>();
+    }
+
+    public void ChangeGraphic(int id)
+    {
+        myImage.sprite = sprites[id];
+    }
+}
