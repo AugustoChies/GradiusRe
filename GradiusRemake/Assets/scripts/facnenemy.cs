7099684a7d62d01a07b86a5209c6e46452ab0abe
11Nim <brunolfar@hotmail.com>
2021-01-06 15:51:40 -0300

    já ajeitei o fan enemy pra morrer tbm

--- GradiusRemake/Assets/scripts/facnenemy.cs
+++ GradiusRemake/Assets/scripts/facnenemy.cs
@@ -4,31 +4,58 @@ using UnityEngine;
 
 public class facnenemy : MonoBehaviour
 {
+    public GlobalStats stats;
     public Vector2 fanDirection = Vector2.zero;
     private float fanSpeed;
     private bool isStarting;
     private Rigidbody2D fan;
-    
+    public bool isItDed;
+    public Animator spriteAnim;
 
-    
     // Start is called before the first frame update
     void Start()
     {
-        
+        isItDed = false; 
         fan = this.GetComponent<Rigidbody2D>();
         isStarting = true;
-        fanDirection.x -= 1;
-        fanSpeed = 3;
+        fanDirection.x -= 10;
+        fanSpeed = 10;
+    }
+
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        Debug.Log("fasfaf");
+        
+        //if(col.gameObject.name == "Ship") //colocar aqui o nome do missil
+            {
+                spriteAnim.SetBool("IsDed", true);
+                isItDed = true;
+                //colocar aqui algo q aumente a pontuação.............................................................................................................
+                StartCoroutine(DedNow());
+                
+                
+            }
+            
+    }
+
+    IEnumerator DedNow()
+    {
+        yield return new WaitForSeconds(0.5f);
+        this.gameObject.SetActive(false); //desliga o objeto
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(isStarting)
+        if(!isItDed)
         {
-            //this.fan.position = {30,15};
-            isStarting = false;
+            if(isStarting)
+            {
+                //this.fan.position = {30,15};
+                isStarting = false;
+            }
+            fan.MovePosition(this.fan.position + fanDirection.normalized * fanSpeed * Time.deltaTime);
         }
-        fan.MovePosition(this.fan.position + fanDirection.normalized * fanSpeed * Time.deltaTime);
     }
 }
