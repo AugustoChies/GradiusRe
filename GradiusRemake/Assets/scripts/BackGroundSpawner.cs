48038856bef1f2c7e66d56cfc880343e13a2bf32
AugustoChies <guto_chies@hotmail.com>
2021-01-14 21:11:25 -0300

    Scrolling background

---
+++ GradiusRemake/Assets/scripts/BackGroundSpawner.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackGroundSpawner : MonoBehaviour
+{
+    public GlobalStats stats;
+    public GameObject backgroundprefab;
+    public Transform scrollerParent;
+    public List<GameObject> currentImages;
+    public Vector3 spawnPosition;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        currentImages = new List<GameObject>();
+        SpawnBackground(new Vector3(0.0f, 0.6f, 0.0f));
+        SpawnBackground(spawnPosition);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (currentImages[1].transform.position.x <= this.transform.position.x) //stars passed midway point
+        {
+            SpawnBackground(spawnPosition);
+            DeleteFifo();
+        }
+    }
+
+    public void SpawnBackground(Vector3 spawn)
+    {
+        GameObject token = Instantiate(backgroundprefab, spawn, Quaternion.identity, scrollerParent);
+        currentImages.Add(token);
+    }
+
+    public void DeleteFifo()
+    {
+        GameObject token = currentImages[0];
+        currentImages.RemoveAt(0);
+        Destroy(token);
+    }
+}
--- GradiusRemake/Assets/scripts/Enemies/Spawner.cs
+++ GradiusRemake/Assets/scripts/Enemies/Spawner.cs
@@ -7,35 +7,33 @@ public class Spawner : MonoBehaviour
     public GlobalStats stats;
     public GameObject spawnable;
     public Transform spawnPosition;
-    private Rigidbody2D rb;
+    //private Rigidbody2D rb;
     public bool backSpawn; //if true, enemy spawns from the back
     public SpriteRenderer visualRepresentation;// for stage building purposes
     // Start is called before the first frame update
     void Start()
     {
         visualRepresentation.enabled = false;
-        rb = this.GetComponent<Rigidbody2D>();
+        //rb = this.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        rb.MovePosition(rb.position + Vector2.left * stats.scrollSpeed * Time.deltaTime);
+        //transform.Translate(Vector2.left * stats.scrollSpeed * Time.deltaTime);
+        // rb.MovePosition(rb.position + Vector2.left * stats.scrollSpeed * Time.deltaTime);
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        Debug.Log("ha");
         if (other.gameObject.layer == 8)//camborder
         {
-            Debug.Log("ha");
             if (backSpawn && other.transform.position.x > this.transform.position.x)
             {
                 Instantiate(spawnable, spawnPosition.position, Quaternion.identity);
             }
             else if (!backSpawn && other.transform.position.x < this.transform.position.x)
             {
-                Debug.Log("ha");
                 Instantiate(spawnable, spawnPosition.position, Quaternion.identity);
             }
         }
